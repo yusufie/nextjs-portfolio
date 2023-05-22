7dc8767b81981e7bc44616cd7126080451e6d135
yusuf <yusufpirejman@gmail.com>
2023-05-23 00:50:28 +0300

    update: media query

--- src/app/page.js
+++ src/app/page.js
@@ -3,26 +3,14 @@ import styles from '/src/app/page.module.css';
 
 function Home() {
 
-  const homeStyle = {
-    color: 'white',
-    marginTop: '5rem',
-    marginBottom: '5rem',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    textAlign: 'center',
-    height: '30vh',
-  };
-
   return (
 
-    <div style={homeStyle}>
+    <div className={styles.home}>
       <h1 className={styles.sectionTitle}>
-      Hello World!
-      <br />
-      <br />
-      I&apos;m Yusuf
+        Hello World!
+        <br />
+        <br />
+        I&apos;m Yusuf
       </h1>
     </div>
 
