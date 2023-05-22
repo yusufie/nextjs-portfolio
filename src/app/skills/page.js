b2639920f631f878793833120864fe05b0e00e94
yusuf <yusufpirejman@gmail.com>
2023-05-22 21:17:29 +0300

    update: backend

--- src/app/skills/page.js
+++ src/app/skills/page.js
@@ -9,30 +9,28 @@ import Link from "next/link";
 function Skills() {
   return (
     <>
-    <section className="skills">
-      <div className={styles.sectionSkills}>
-        <h1 className={styles.sectionTitle}>skills</h1>        
+      <section className="skills">
 
-        <div className={styles.skillCard}>
-          <div className={styles.languageCard}>
-            {skillData.skills.map((skill) => (
-              <Link
-                key={skill.id}
-                href={skill.url}
-                target="_blank"
-                className={styles.skillsItem}
-              >
-                <img
-                  src={skill.logo}
-                  alt={skill.alt}
-                  className={styles.skillCardImage}
-                />
-              </Link>
-            ))}
-          </div>
+        <h1 className={styles.sectionTitle}>skills</h1>
+
+        <div className={styles.sectionSkills}>
+              {skillData.skills.map((skill) => (
+                <Link
+                  key={skill.id}
+                  href={skill.url}
+                  target="_blank"
+                  className={styles.skillsItem}
+                >
+                  <img
+                    src={skill.logo}
+                    alt={skill.alt}
+                    className={styles.skillCardImage}
+                  />
+                </Link>
+              ))}
         </div>
-      </div>
-    </section>
+        
+      </section>
     </>
   );
 }
