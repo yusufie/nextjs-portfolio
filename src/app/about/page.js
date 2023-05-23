be2db8dfbf48c4143e6c82719d5ee8cd0007d986
yusuf <yusufpirejman@gmail.com>
2023-05-23 15:01:55 +0300

    feature: iframe

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,39 +1,56 @@
-import React from 'react'
-
-import aboutData from "/public/datas/aboutData.json"
-
-import styles from "./page.module.css"
-import "../globals.css"
+"use client"
+import React, { useState } from 'react';
+import aboutData from '/public/datas/aboutData.json';
+import styles from './page.module.css';
+import '../globals.css';
 import Link from 'next/link';
 
-
 function About() {
+  const [previewUrl, setPreviewUrl] = useState('');
+
+  const handleMouseEnter = (preview) => {
+    setPreviewUrl(preview);
+  };
+
+  const handleMouseLeave = () => {
+    setPreviewUrl('');
+  };
 
   return (
     <>
       <section className="about">
         <div className={styles.sectionAbout}>
           <h1 className={styles.sectionTitle}>about</h1>
-
-          <div className={styles.skillCard}>
-
+          
             <div className={styles.profileCard}>
               {aboutData.profiles.map((profile) => (
                 <Link key={profile.id} href={profile.url} target="_blank" className={styles.skillsItem}>
-                  <img
-                    src={profile.logo}
-                    alt={profile.alt}
-                    className={styles.skillCardImage}
-                  />
+                  <div
+                    className={styles.skillCardWrapper}
+                    onMouseEnter={() => handleMouseEnter(profile.preview)}
+                    onMouseLeave={handleMouseLeave}
+                  >
+                    <img
+                      src={profile.logo}
+                      alt={profile.alt}
+                      className={styles.skillCardImage}
+                    />
+                    
+                    {previewUrl === profile.preview && (
+                      <div className={styles.previewOverlay}>
+
+                        <img src={profile.preview} className={styles.previewImage}/>
+
+                      </div>
+                    )}
+                  </div>
                 </Link>
               ))}
             </div>
-
           </div>
-        </div>
       </section>
     </>
   );
 }
 
-export default About
+export default About;
