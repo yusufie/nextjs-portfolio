bf2819552f608ad1faca5513153b823afcc1abc0
yusuf <yusufpirejman@gmail.com>
2023-06-19 14:33:37 +0300

    update: section title

--- src/app/page.js
+++ src/app/page.js
@@ -6,12 +6,23 @@ function Home() {
   return (
 
     <div className={styles.home}>
+
       <h1 className={styles.sectionTitle}>
+
         Hello World!
+
         <br />
         <br />
+
         I&apos;m Yusuf
+
+        <br />
+        <br />
+        
+        a web developer
+
       </h1>
+
     </div>
 
   );
